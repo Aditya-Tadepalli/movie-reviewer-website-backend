27af62c30118cfd14ee3853314870e48c1b0a8cf
Aditya Tadepalli <ssa.tadepalli@gmail.com>
2024-09-29 12:54:05 -0500

    Added backend

---
+++ src/main/java/dev/aditya/movies/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.aditya.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
